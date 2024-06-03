c6f151a3fd6090029ee691be4f20784fbe2229d9
Andreas Bauer <andreas.bauer@stanford.edu>
2024-06-03 15:55:33 +0200

    Fix swiftlint

--- Sources/BluetoothServices/Characteristics/WeightScaleFeature.swift
+++ Sources/BluetoothServices/Characteristics/WeightScaleFeature.swift
@@ -197,6 +197,7 @@ public struct WeightScaleFeature: OptionSet {
     ///   - heightResolution: The resolution for height values for a ``WeightMeasurement``.
     ///   - options: Additional flags and options of ``WeightScaleFeature`` (e.g., BMI support or multi user support).
     public init(weightResolution: WeightResolution, heightResolution: HeightResolution = .unspecified, options: WeightScaleFeature...) {
+        // swiftlint:disable:previous function_default_parameter_at_end
         let rawValue = (UInt32(weightResolution.rawValue) << 3) | (UInt32(heightResolution.rawValue) << 7)
         self = WeightScaleFeature(rawValue: rawValue).union(WeightScaleFeature(options))
     }
