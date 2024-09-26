908430a5cf74e95dcf143c95657acd52a21b953f
Andreas Bauer <andreas.bauer@stanford.edu>
2024-09-26 12:37:34 +0200

    Add some useful API

--- Sources/SpeziBluetooth/Configuration/Apperance/DeviceAppearance.swift
+++ Sources/SpeziBluetooth/Configuration/Apperance/DeviceAppearance.swift
@@ -22,3 +22,40 @@ public enum DeviceAppearance {
 
 
 extension DeviceAppearance: Hashable, Sendable {}
+
+
+extension DeviceAppearance {
+    /// Retrieve the appearance for a device.
+    /// - Parameter variantPredicate: If the device has different variants, this predicate will be used to match the desired variant.
+    /// - Returns: Returns the device `appearance` and optionally the `variantId`, if the appearance of a variant was returned.
+    public func appearance(where variantPredicate: (Variant) -> Bool) -> (appearance: Appearance, variantId: String?) {
+        switch self {
+        case let .appearance(appearance):
+            (appearance, nil)
+        case let .variants(defaultAppearance, variants):
+            if let variant = variants.first(where: variantPredicate) {
+                (Appearance(name: variant.name, icon: variant.icon), variant.id)
+            } else {
+                (defaultAppearance, nil)
+            }
+        }
+    }
+    
+    /// Retrieve the icon appearance of a device.
+    /// - Parameter variantId: The optional variant id to query. This id will be used to selected the device variant, if the device declares different variant appearances.
+    /// - Returns: Returns the device icon.
+    public func deviceIcon(variantId: String?) -> ImageReference {
+        appearance { variant in
+            variant.id == variantId
+        }.appearance.icon
+    }
+
+    /// Retrieve the name of a device.
+    /// - Parameter variantId: The optional variant id to query. This id will be used to selected the device variant, if the device declares different variant appearances.
+    /// - Returns: Returns the device name.
+    public func deviceName(variantId: String?) -> String {
+        appearance { variant in
+            variant.id == variantId
+        }.appearance.name
+    }
+}
